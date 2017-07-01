9d2153b410b3f3c2a27f6e012800cc54ae01ed07
shazic <shashank.chattopadhyaya@gmail.com>
2017-07-02 02:02:03 +0530

    Added a bat file to reset the ERRORLEVEL to zero.
    Added reseterrorlevel.bat.

--- dos/reseterrorlevel.bat
+++ dos/reseterrorlevel.bat
@@ -4,27 +4,19 @@
 @rem $$                         PURPOSE                             $$
 @rem $$ ----------------------------------------------------------  $$
 @rem $$                                                             $$
-@rem $$  SETS THE TIMESTAMP IN THE FORMAT D-yyyy-mm-dd-T_hh_mm_ss   $$
-@rem $$  AND STORED IN THE VARIABLE TS                              $$
+@rem $$  RESET THE ERRORLEVEL SYSTEM VARIABLE TO ZERO.              $$
 @rem $$                                                             $$
 @rem $$ ----------------------------------------------------------  $$
 @rem $$                          SYNTAX                             $$
 @rem $$ ----------------------------------------------------------  $$
 @rem $$                                                             $$
-@rem $$  SETTIMESTAMP                                               $$
+@rem $$  RESETERRORLEVEL                                            $$
 @rem $$ ----------------------------------------------------------  $$
 @rem $$                          OUTPUT                             $$
 @rem $$ ----------------------------------------------------------  $$
 @rem $$                                                             $$
-@rem $$  Variable: %ts%                                             $$
-@rem $$            with date in the format D-yyyy-mm-dd-T_hh_mm_ss  $$
+@rem $$  NONE.                                                      $$
 @rem $$                                                             $$
 @rem $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
-@rem reformat the timestamp
-@for /f %%I in ('wmic os get localdatetime ^|find "20"') do @set dt=%%I
-@rem date format is now YYYYMMDDhhmmss...
-
-@set ts=D-%dt:~0,4%-%dt:~4,2%-%dt:~6,2%-T_%dt:~8,2%_%dt:~10,2%_%dt:~12,2%
-
-@rem echo %ts%
+exit /b 0
