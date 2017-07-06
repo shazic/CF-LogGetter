0053442b9d17d5e6aa66da136d4575c16de684e4
Shashank Chattopadhyaya <shazic@users.noreply.github.com>
2017-07-06 16:03:15 +0530

    The Output is now produced without headers.
    All records that start with a # symbol are now removed from the output file.

--- dos/rmvhdrs.bat
+++ dos/rmvhdrs.bat
@@ -1,10 +1,36 @@
+@rem $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+@rem $$                                                             $$
+@rem $$ ----------------------------------------------------------  $$
+@rem $$                         PURPOSE                             $$
+@rem $$ ----------------------------------------------------------  $$
+@rem $$                                                             $$
+@rem $$  REMOVES HEADER RECORDS FROM THE CONSOLIDATED LOG FILE.     $$
+@rem $$  THIS BAT FILE REMOVES THE RECORDS THAT STARTS WITH A #     $$
+@rem $$  SYMBOL AT THE FIRST POSITION.                              $$
+@rem $$                                                             $$
+@rem $$ ----------------------------------------------------------  $$
+@rem $$                          SYNTAX                             $$
+@rem $$ ----------------------------------------------------------  $$
+@rem $$                                                             $$
+@rem $$  RMVHDRS     INPUT_CONSOLIDATED_LOG_FILE     OUTPUT         $$
+@rem $$ ----------------------------------------------------------  $$
+@rem $$                          OUTPUT                             $$
+@rem $$ ----------------------------------------------------------  $$
+@rem $$                                                             $$
+@rem $$  FILE:                                                      $$
+@rem $$              THE CONTENT IS SAME AS THE INPUT FILE, WITHOUT $$
+@rem $$              THE HEADER RECORDS.                            $$
+@rem $$                                                             $$
+@rem $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
 @echo off
 SETLOCAL DisableDelayedExpansion
 FOR /F "usebackq delims=" %%a in (`"findstr /n ^^ %1"`) do (
     set "var=%%a"
     SETLOCAL EnableDelayedExpansion
     set "var=!var:*:=!"
-    echo(!var! >> %2
+    set "frstsmbl=!var:~0,1!"
+    if "!frstsmbl!" NEQ "#" echo(!var! >> %2
     ENDLOCAL
 )
 )
